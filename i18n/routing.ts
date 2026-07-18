6b1525d5a7edde42ed768527514d8bda8a36b313
Jophkins <sergebrew@gmail.com>
2026-07-18 13:24:59 -0600

    :hammer: fix: default language changed to en

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["ru", "en"],
-  defaultLocale: "ru",
+  defaultLocale: "en",
 });
 
 export type Locale = (typeof routing.locales)[number];
